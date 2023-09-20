fc3b2febfd27f03546956c2595d5206f016d6a38
Olayor-X <latubosun4328@gmail.com>
2023-09-20 18:20:51 +0100

    git diff

--- validate.php
+++ validate.php
@@ -1,10 +1,10 @@
 <?php
 function validate($data){
-    $code = "123456789";
     $data= trim($data);
     $data = stripslashes($data);
     $data= htmlspecialchars($data);
 
     return $data;
 }
+$code = "123456789";
 ?>
